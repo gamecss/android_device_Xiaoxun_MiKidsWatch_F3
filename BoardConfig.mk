8a18c4d469fadcc613a680b091b5ed62198aa67c
gamecss <1156393759@qq.com>
2022-10-25 14:42:40 +0800

    Initial device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,98 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#device_path
+DEVICE_PATH := device/Xiaoxun/MiKidsWatch_F3
+
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+TARGET_BOARD_PLATFORM := mt6739
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MT6735
+
+# MiKidsWatch_F3 kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x05000000
+BOARD_TAGS_OFFSET := 0x04000000
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32S1,32S1 androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+
+# allow building ramdisk(s) with lzma
+#LZMA_RAMDISK_TARGETS += recovery
+
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_USES_MTK_HARDWARE := true
+MTK_HARDWARE := true
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+#BOARD_USES_FULL_RECOVERY_IMAGE := true
+BOARD_FLASH_BLOCK_SIZE := 0
+TARGET_KMODULES := true
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := MiKidsWatch_F3
+
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+#Recovery FSTAB
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/recovery.fstab
+
+# TWRP stuff
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXTRA_LANGUAGES := true
+TW_THEME := watch_mdpi
+DEVICE_RESOLUTION := 320x360
+#TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 100
+TW_INCLUDE_FB2PNG := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_NO_CPU_TEMP := true
+TW_REBOOT_RECOVERY := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_HAVE_SELINUX := true
+TW_DEFAULT_LANGUAGE := en
+TW_HAS_MTP := true
+TW_MTP_DEVICE := /dev/mtp_usb
+#use toolbox
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_BASH := true
+TW_EXCLUDE_TZDATA := true
+TW_EXCLUDE_NANO := true
---
+++ omni_MiKidsWatch_F3.mk
@@ -0,0 +1,44 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#device_path
+DEVICE_PATH := device/Xiaoxun/MiKidsWatch_F3
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device
+PRODUCT_DEVICE := MiKidsWatch_F3
+PRODUCT_NAME := full_MiKidsWatch_F3
+PRODUCT_BRAND := Xiaoxun
+PRODUCT_MODEL := MiKidsWatch_F3
+PRODUCT_MANUFACTURER := Xiaoxun
+
+# Prebuilt kernel
+PRODUCT_COPY_FILES += device/Xiaoxun/MiKidsWatch_F3/prebuilt/kernel:kernel
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    $(DEVICE_PATH)/recovery/root/init.recovery.mt6739.rc:root/init.recovery.mt6739.rc \
+    $(DEVICE_PATH)/recovery/root/ueventd.mt6739.rc:root/ueventd.mt6739.rc \
+    $(DEVICE_PATH)/recovery/root/init.recovery.usb.rc:root/init.recovery.usb.rc \
+    $(DEVICE_PATH)/recovery/root/init.recovery.service.rc:root/init.recovery.service.rc
+
+#Allow_missing_dependencies
+ALLOW_MISSING_DEPENDENCIES=true
